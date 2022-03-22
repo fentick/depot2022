fe2f11d6bfb98b0864ed669e22e85fa9329d3a07
Fentick <fentick_xv@msn.com>
2022-03-22 15:06:05 +0800

    Validation!

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,12 @@
 class Product < ApplicationRecord
+  validates :title, :description, :image_url, presence: { message: 'must be given please.'}
+  
+  validates :title, uniqueness: true
+
+  validates :price, numericality: { greater_than_or_equal_to: 0.01}
+  
+  validates :image_url, allow_blank: true, format: { 
+    with: %r{\.(gif|jpg|png)\z}i,
+    message: 'must be a URL for GIF, JPG or PNG image'
+  }
 end
