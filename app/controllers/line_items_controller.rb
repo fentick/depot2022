b5d11d9cb775a047a9885471c287323c9158fc24
Fentick <fentick_xv@msn.com>
2022-04-04 07:45:23 +0800

    Hotwiring the Storefront - F1 Moving the Cart

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -35,7 +35,8 @@ class LineItemsController < ApplicationController
 
    respond_to do |format|
       if @line_item.save
-        format.html {redirect_to cart_url(@line_item.cart) }
+        format.html { redirect_to store_index_url }        
+#        format.html {redirect_to cart_url(@line_item.cart) }
 #        session[:counter] = nil
         store_index_reset
 #        format.html { redirect_to line_item_url(@line_item), notice: "Line item was successfully created." }
