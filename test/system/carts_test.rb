dfedacf099b2d02d3d5a70d2e5c88c89ed95b282
Fentick <fentick_xv@msn.com>
2022-05-09 13:05:13 +0800

    chp12 - G3 playtime - tet to verify the cart visibility

---
+++ test/system/carts_test.rb
@@ -0,0 +1,16 @@
+require "application_system_test_case"
+
+class CartsTest < ApplicationSystemTestCase
+  test "check carts" do
+    visit store_index_url
+
+    click_on 'Add to Cart', match: :first
+
+    assert has_text? 'Your Cart'
+
+    click_on 'Empty Cart'
+
+    assert has_text? 'Your Cart'
+
+  end
+end
