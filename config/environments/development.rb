29a1886a33b9f24df1e6b17eb14a476a9de3325a
Fentick <fentick_xv@msn.com>
2022-05-13 00:15:31 +0800

    chp13 - H1 sending confirmation emails

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,9 +37,27 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  #config.action_mailer.raise_delivery_errors = true
+  #config.action_mailer.perform_deliveries = true
 
   config.action_mailer.perform_caching = false
+
+  #config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  config.action_mailer.delivery_method = :test
+
+  #config.action_mailer.delivery_method = :smtp
+
+  # config.action_mailer.smtp_settings = {
+  #   address: "smtp.exmail.qq.com",
+  #   port: 465,
+  #   domain: "usingnow.com",
+  #   authentication: "login",
+  #   user_name: "",
+  #   password: "",
+  #   enable_starttls_auto: true,
+  #   ssl: true
+  # }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
